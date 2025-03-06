74442bd8ae9349120e2239bb7bff966d5b99146e
JinukMoon <jumoon@snu.ac.kr>
2025-03-06 13:18:01 +0900

    GNN_MLP

--- catbench/__init__.py
+++ catbench/__init__.py
@@ -1,8 +1,8 @@
 from .core import (
     execute_benchmark,
     execute_benchmark_single,
-    analysis_GNNs,
-    analysis_GNNs_single,
+    analysis_MLPs,
+    analysis_MLPs_single,
     process_output,
     cathub_preprocess,
     userdata_preprocess,
